c0f2fa18a2657676a466980e800741b3872ecdf6
Antoinette <create@aplusmedia.info>
2019-10-04 13:48:44 -0400

    what i've done so far

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -17,3 +17,46 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+let card = document.querySelector("card")
+axios
+    .get("https://lambda-times-backend.herokuapp.com/articles")
+    .then(response => {
+        console.log(response.data.articles.bootstrap)
+        response.data.articles.bootstrap.forEach(data =>{
+           const newData = response.data.articles.bootstrap;
+           console.log(newData)
+            card.appendChild(myArticle(newData))
+        })
+    })
+    .catch(error =>{
+        console.log("Error", error)
+    })
+
+    function myArticle(newData){
+        const card = document.createElement("div")
+        card.classList.add("card")
+
+        const headline = document.createElement("div")
+        headline.classList.add("headline")
+        headline.textContent = newData.headline;
+        card.appendChild(headline)
+
+        const author = document.createElement("div")
+        author.classList.add("author")
+        
+        const placeHolder = document.createElement("div")
+        placeHolder.classList.add("img-container")
+        author.appendChild(placeHolder)
+        
+        const pic = document.createElement("img")
+        pic.setAttribute("src", date.articles.bootstrap[0].authorPhoto)
+        author.appendChild(pic)
+
+        const name = document.createElement("span")
+        name.textContent = `By ${newData.authorName}`;
+        author.appendChild(name)
+
+        card.appendChild(author)
+
+        return card;
+    }
